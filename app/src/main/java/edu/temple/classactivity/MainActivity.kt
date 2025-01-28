89368f1cde6fbe51938d67bb58030504e2544140
Centrous <maxwellchanetekimang@gmail.com>
2025-01-28 12:10:38 -0500

    with the added click button, added a setonclicklistener, so that when you click the button it'll say "Button Clicked!"

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,22 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var clickMeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (findViewById<Button>(R.id.button)!!).setOnClickListener {
+            (it as Button).text = "Button Clicked!"
+        }
     }
 }
